4ea797e6d0789598585a5a2b5bff451fdb6dbeb0
shellew <nishioka2013_1124@yahoo.co.jp>
2023-09-18 18:42:03 +0900

    signup,login,logout機能

--- naming_app/naming_pre_app/views.py
+++ naming_app/naming_pre_app/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home') #ユーザーをホームページにリダイレクト
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home') #ユーザーをホームページにリダイレクト
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home') #ログアウト後、ホームページにリダイレクト
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
